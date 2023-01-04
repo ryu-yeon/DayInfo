8b026b427a59b07bd55ae1c2bb4c175b93ca32c7
ryu-yeon <rytak108@gmail.com>
2023-01-05 04:56:22 +0900

    오타 수정

--- DayInfo/App/Scene/MainView.swift
+++ DayInfo/App/Scene/MainView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         HomeView()
+    }
 }
-
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
     }
 }
+
