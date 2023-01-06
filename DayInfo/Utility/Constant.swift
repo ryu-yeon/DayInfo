23e9fcb82f20b1ca355331d4958c75dd53056ee7
ryu-yeon <rytak108@gmail.com>
2023-01-07 04:47:44 +0900

    feedback 디데이계산 함수 추가

--- DayInfo/Utility/Constant.swift
+++ DayInfo/Utility/Constant.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 let sampleWeather = Weather(weather: [WeatherBasic(id: 803, main: "Clouds", description: "튼구름", icon: "04d")],
                             main: WeatherMain(temp: -8.27, feels_like: -12.47, temp_min: -7.22, temp_max: -13.51, pressure: 1033, humidity: 53))
-let sampleTodo = Todo(title: "장보기", detail: "라면, 양파, 고기", date: "", color: "", icon: "circle", done: false)
 
 let gridSize = (UIScreen.main.bounds.width - 60) / 2
 
 let basicColor = [
     "#FF0000", "#008000", "#0000FF", "#FFA500", "#808080", "#000000"
 ]
+
+let feedback = UIImpactFeedbackGenerator(style: .medium)
+
+func calculateDate(date: Date) -> String {
+    let interval = Date().timeIntervalSince(date)
+    let days = Int(floor(interval / 86400))
+    
+    if days > 0 {
+        return "D+\(days)"
+    } else if days < 0 {
+        return "D\(days)"
+    } else {
+        return "D-DAY"
+    }
+}
