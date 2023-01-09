99b9ee4911b31f3414398aea0ed615c1c51a2d6c
ryu-yeon <rytak108@gmail.com>
2023-01-09 12:24:26 +0900

    UI 수정

--- DayInfo/App/Scene/Setting/SettingView.swift
+++ DayInfo/App/Scene/Setting/SettingView.swift
@@ -12,7 +12,7 @@ struct SettingView: View {
         VStack(alignment: .center, spacing: 8) {
             GroupBox(
                 label:
-                    SettingLabelView(labelText: "DayInfo", labelImage: "info.circle")
+                    SettingLabelView(labelText: "심플한 투두", labelImage: "info.circle")
             ) {
                 Divider().padding(.vertical, 4)
                 
@@ -23,7 +23,7 @@ struct SettingView: View {
                         .frame(width: 80, height: 80)
                         .cornerRadius(9)
                     
-                    Text("심플한 디자인의 할일 목록 앱입니다.\n상단에서 현재위치의 날씨와 설정한 디데이를 볼수 있습니다. 할일 목록을 추가하고 색상, 목표일을 지정할수 있습니다.")
+                    Text("심플한 디자인의 할 일 목록 앱입니다.\n상단에서 현재위치의 날씨와 설정한 디데이를 볼 수 있습니다. 할 일 목록을 추가하고 색상, 목표 일을 지정할 수 있습니다.")
                         .font(.footnote)
                 }
                 
